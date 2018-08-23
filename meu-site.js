9cb7cbdc10b522aa55335be265f0ec8c06c2746c
Renê-Boaventura-Neto <reneboaventuraneto@gmail.com>
2018-08-23 17:02:22 -0300

    tidy js file

--- meu-site.js
+++ meu-site.js
@@ -1,3 +1,4 @@
+// js for navbar to desapear on scroll
 let lastScrollTop = 0;
 window.addEventListener("scroll", function(){ 
   let st = window.pageYOffset || document.documentElement.scrollTop;
@@ -6,12 +7,10 @@ window.addEventListener("scroll", function(){
    } else {
     document.getElementById("navbar").style.top = "0";
    }
-
-
-
    lastScrollTop = st <= 0 ? 0 : st;
 }, false);
 
+// js for navbar to desapear when clicking outside of it
 window.addEventListener('click', function(event){
   const menu = document.getElementById('movelmenu');
   const toggler = document.getElementsByClassName('navbar-toggler');
