bf9ef396b059e11345d4dad5cbc92ca264e881b4
Renê-Boaventura-Neto <reneboaventuraneto@gmail.com>
2018-08-28 15:04:57 -0300

    attempt to fix menu toggle bug

--- meu-site.js
+++ meu-site.js
@@ -10,11 +10,55 @@ window.addEventListener("scroll", function(){
    lastScrollTop = st <= 0 ? 0 : st;
 }, false);
 
+/* let lastScrollTop = 0;
+window.addEventListener("click", function(event){ 
+  const menu = document.getElementById('navbar');
+  const togglerIcon = document.getElementsByClassName('navbar-toggler-icon');
+  const toggler = document.getElementsByClassName('navbar-toggler');
+	if (event.target == togglerIcon && showing){
+     document.getElementById("movelmenu").style.top = "-300px";
+   } else if (event.target == togglerIcon ){
+    document.getElementById("movelmenu").style.top = "0";
+   }
+
+}); */
+
+
+
+
+
+
 // js for navbar to desapear when clicking outside of it
+/* window.addEventListener('click', function(event){
+  const menu = document.getElementById('navbar');
+  const movelmenu = document.getElementById('movelmenu');
+  const toggler = document.getElementsByClassName('navbar-toggler');
+  const togglerIcon = document.getElementsByClassName('navbar-toggler-icon');
+	if (event.target != menu && event.target.parentNode != menu  && event.target != togglerIcon[0]){
+        toggler[0].click();
+        //movelmenu.style.display = 'none';
+        console.log(event.target);
+    }
+}); */
+
+//console.log(event.target)
+
+//menu.style.display = 'none'
+//event.target != toggler[0]
+
 window.addEventListener('click', function(event){
-  const menu = document.getElementById('movelmenu');
+  const menu = document.getElementById('navbar');
+  const movelmenu = document.getElementById('movelmenu');
+  //const toggler = document.getElementsByClassName('navbar-toggler');
+	if (event.target != menu && event.target.parentNode != menu){
+    movelmenu.classList.remove("show");
+    }
+});
+
+/* window.addEventListener('click', function(event){
+  const menu = document.getElementById('navbar');
   const toggler = document.getElementsByClassName('navbar-toggler');
 	if (event.target != menu && event.target.parentNode != menu){
         toggler[0].click();
     }
-});
+}); */
